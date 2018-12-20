549e81c0a1140b391e893cbb0d950aa8208dfadc
jondaa <36964678+jondaa@users.noreply.github.com>
2018-12-20 11:18:36 +0200

    a

--- hw2/models.py
+++ hw2/models.py
@@ -168,7 +168,7 @@ class YourCodeNet(ConvClassifier):
                 in_channels = self.filters[conv_num]
                 conv_num += 1
                 layers.append(torch.nn.ReLU())
-            layers.append(torch.nn.FractionalMaxPool2d(3, output_ratio=0.70711)
+            layers.append(torch.nn.FractionalMaxPool2d(3, output_ratio=0.70711))
         layers.append(torch.nn.Conv2d(in_channels, in_channels, 4))
         layers.append(torch.nn.ReLU())                  
         layers.append(torch.nn.Conv2d(in_channels, in_channels, 8))     
