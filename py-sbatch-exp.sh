bdce15e041751ac28304e4e508a9b3ac43b0c420
jondaa <36964678+jondaa@users.noreply.github.com>
2018-12-20 14:52:14 +0200

    Edit run all

--- py-sbatch-exp.sh
+++ py-sbatch-exp.sh
@@ -69,20 +69,20 @@ python -m hw2.experiments run-exp -n exp1_1_K64_L6 -K 64 -L 6 -P 2 -H 100
 python -m hw2.experiments run-exp -n exp1_1_K64_L8 -K 64 -L 8 -P 2 -H 100
 
 
-python -m hw2.experiments run-exp -n exp1_2_K32_L2 -K 32 -L 2 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_2_K64_L2 -K 64 -L 2 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_2_K128_L2 -K 128 -L 2 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_2_K256_L2 -K 256 -L 2 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L2_K32 -K 32 -L 2 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L2_K64 -K 64 -L 2 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L2_K128 -K 128 -L 2 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L2_K256 -K 256 -L 2 -P 2 -H 100
 
-python -m hw2.experiments run-exp -n exp1_4_K32_L2 -K 32 -L 4 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_4_K64_L2 -K 64 -L 4 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_4_K128_L2 -K 128 -L 4 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_4_K256_L2 -K 256 -L 4 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L4_K32 -K 32 -L 4 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L4_K64 -K 64 -L 4 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L4_K128 -K 128 -L 4 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L4_K256 -K 256 -L 4 -P 2 -H 100
 
-python -m hw2.experiments run-exp -n exp1_8_K32_L2 -K 32 -L 8 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_8_K64_L2 -K 64 -L 8 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_8_K128_L2 -K 128 -L 8 -P 2 -H 100
-python -m hw2.experiments run-exp -n exp1_8_K256_L2 -K 256 -L 8 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L8_K32 -K 32 -L 8 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L8_K64 -K 64 -L 8 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L8_K128 -K 128 -L 8 -P 2 -H 100
+python -m hw2.experiments run-exp -n exp1_2_L8_K256 -K 256 -L 8 -P 2 -H 100
 
 
 python -m hw2.experiments run-exp -n exp1_3_L1_K64-128-256 -K 64 128 256 -L 1 -P 2 -H 100
